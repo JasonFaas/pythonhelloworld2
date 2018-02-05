8cd80365ef6f23d111cb40d730e7911b9eb648aa
JasonFaas <jason.a.faas@gmail.com>
2018-02-04 21:39:50 -0800

    Still loading documents

--- src/coursera/intro_to_data_science/assignment3/assignment3.py
+++ src/coursera/intro_to_data_science/assignment3/assignment3.py
@@ -20,16 +20,6 @@ energy = pd.read_excel(
     na_values='...'
 )
 energy['Energy Supply'] *= 1000 * 1000
-
-
-def updateCountry(param):
-    print(type(param))
-    param = param[0:param.find('(')]
-
-    return param.strip()
-
-
-# energy['Country'] = updateCountry(energy['Country'])
 energy['Country'] = energy['Country'].replace("Republic of Korea", "South Korea")
 energy['Country'] = energy['Country'].replace("United States of America", "United States")
 energy['Country'] = energy['Country'].replace("United Kingdom of Great Britain and Northern Ireland", "United Kingdom")
@@ -37,28 +27,21 @@ energy['Country'] = energy['Country'].replace("China, Hong Kong Special Administ
 energy['Country'] = energy['Country'].str.replace(r'\d+', '')
 energy['Country'] = energy['Country'].str.replace(r'\(.*\)', '')
 energy['Country'] = energy['Country'].str.strip()
-print(energy)
-#index_col=0, skiprows=1)
+#print(energy)
 
-# for col in energy.columns:
-#     if col[:2] == '01':
-#         energy.rename(columns={col: 'Gold' + col[4:]}, inplace=True)
-#     if col[:2] == '02':
-#         energy.rename(columns={col: 'Silver' + col[4:]}, inplace=True)
-#     if col[:2] == '03':
-#         energy.rename(columns={col: 'Bronze' + col[4:]}, inplace=True)
-#     if col[:1] == '№':
-#         energy.rename(columns={col: '#' + col[1:]}, inplace=True)
-#
-# names_ids = energy.index.str.split('\s\(')  # split the index by '('
-#
-# energy.index = names_ids.str[0]  # the [0] element is the country name (new index)
-# energy['ID'] = names_ids.str[1].str[:3]  # the [1] element is the abbreviation or ID (take first 3 characters from that)
-#
-# energy = energy.drop('Totals')
-# energy.head()
-#
-#
+gdp = pd.read_csv('world_bank.csv', skiprows=4)
+gdp['Country Name'] = gdp['Country Name'].replace("Korea, Rep.", "South Korea")
+gdp['Country Name'] = gdp['Country Name'].replace("Iran, Islamic Rep.", "Iran")
+gdp['Country Name'] = gdp['Country Name'].replace("Hong Kong SAR, China", "Hong Kong")
+#print(gdp)
+
+ScimEn = pd.read_excel(
+    'scimagojr-3.xlsx',
+    sheet_name='Sheet1'
+)
+
+print(ScimEn)
+
 # def answer_one():
 #     return energy['Gold'].idxmax()
 #
