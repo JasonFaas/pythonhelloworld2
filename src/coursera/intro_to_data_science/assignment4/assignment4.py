40e760da47d2f2af7f70bb860984bf8248d1e2a1
JasonFaas <jason.a.faas@gmail.com>
2018-02-14 15:41:56 -0800

    assignment 4: add region helper

--- src/coursera/intro_to_data_science/assignment4/assignment4.py
+++ src/coursera/intro_to_data_science/assignment4/assignment4.py
@@ -11,8 +11,25 @@ def hello_world():
 print(hello_world())
 
 
-# census_df = pd.read_csv('university_towns.txt')
-#census_df.head()
+
+def get_region_info():
+    columns_to_keep = ['RegionID','RegionName','State','Metro','CountyName']
+    census_df = pd.read_csv('City_Zhvi_AllHomes.csv')
+    census_df = census_df[columns_to_keep]
+    # print(city_and_region_verifier("AL", "Auburn", census_df))
+    # print(city_and_region_verifier("AL", "whatwhatwhat", census_df))
+    # print(city_and_region_verifier("KA", "Auburn", census_df))
+    # print(auburn_)
+    return census_df
+
+
+def city_and_region_verifier(state, region_name, census_df):
+    auburn_ = census_df.loc[census_df['RegionName'] == region_name]
+    al_ = len(auburn_.loc[auburn_['State'] == state]) == 1
+    return al_
+
+
+print("A0:" + str(get_region_info().head()))
 
 
 def get_list_of_university_towns():
@@ -26,16 +43,18 @@ def get_list_of_university_towns():
     1. For "State", removing characters from "[" to the end.
     2. For "RegionName", when applicable, removing every character from " (" to the end.
     3. Depending on how you read the data, you may need to remove newline character '\n'. '''
+    region_info = get_region_info()
 
     f = open('university_towns.txt', 'r')
     message = f.read()
     f.close()
     split = message.split('\n')
 
-    print(len(split))
+    # print(len(split))
 
     df = pd.DataFrame(columns=["State", "RegionName"])
     state = 'what'
+    counter = 0
     for city_or_state in split:
         if str.strip(city_or_state).endswith('[edit]'):
             state = str.strip(city_or_state)
@@ -43,10 +62,18 @@ def get_list_of_university_towns():
             # print(state)
         elif city_or_state.__contains__(" ("):
             city = city_or_state[0:city_or_state.index(" (")]
-            df = df.append(pd.DataFrame([ [state, city]],columns=["State", "RegionName"]))
-
+            if city.__contains__(','):
+                # print("WHAT::" + city)
+                city = city[0:city_or_state.index(",")]
+            # else:
+            df.loc[counter] = [state, city]
+            counter += 1
+        elif city_or_state.__contains__(', South Central College'):
+            city = city_or_state[0:city_or_state.index(", South Central College")]
+            df.loc[counter] = [state, city]
+            counter += 1
         # else:
-            # print("Subcontext::" + city_or_state)
+        #     print("Subcontext::" + city_or_state)
 
     # df
 
@@ -54,8 +81,75 @@ def get_list_of_university_towns():
     # columns=["State", "RegionName"])
     # what_df.
     # what_df.append(["Michigan", "Yipsilanti"] )
-
-
+    # bad_df = pd.DataFrame([["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"]],
+    #           columns=["State", "RegionName"])
+    # bad_df = bad_df.
+    # print(len(df))
     return df
 
 print("A1:" + str(get_list_of_university_towns().head()))
+
+
+def get_recession_start():
+    '''Returns the year and quarter of the recession start time as a
+    string value in a format such as 2005q3'''
+
+    return "ANSWER"
+
+print("A2:" + str(get_recession_start()))
+
+
+def get_recession_end():
+    '''Returns the year and quarter of the recession end time as a
+    string value in a format such as 2005q3'''
+
+    return "ANSWER"
+
+print("A3:" + str(get_recession_end()))
+
+
+def get_recession_bottom():
+    '''Returns the year and quarter of the recession bottom time as a
+    string value in a format such as 2005q3'''
+
+    return "ANSWER"
+
+
+print("A4:" + str(get_recession_bottom()))
+
+
+def convert_housing_data_to_quarters():
+    '''Converts the housing data to quarters and returns it as mean
+    values in a dataframe. This dataframe should be a dataframe with
+    columns for 2000q1 through 2016q3, and should have a multi-index
+    in the shape of ["State","RegionName"].
+
+    Note: Quarters are defined in the assignment description, they are
+    not arbitrary three month periods.
+
+    The resulting dataframe should have 67 columns, and 10,730 rows.
+    '''
+
+    return "ANSWER"
+
+print("A5:" + str(convert_housing_data_to_quarters()))
+
+
+def run_ttest():
+    '''First creates new data showing the decline or growth of housing prices
+    between the recession start and the recession bottom. Then runs a ttest
+    comparing the university town values to the non-university towns values,
+    return whether the alternative hypothesis (that the two groups are the same)
+    is true or not as well as the p-value of the confidence.
+
+    Return the tuple (different, p, better) where different=True if the t-test is
+    True at a p<0.01 (we reject the null hypothesis), or different=False if
+    otherwise (we cannot reject the null hypothesis). The variable p should
+    be equal to the exact p value returned from scipy.stats.ttest_ind(). The
+    value for better should be either "university town" or "non-university town"
+    depending on which has a lower mean price ratio (which is equivilent to a
+    reduced market loss).'''
+
+    return "ANSWER"
+
+print("A6:" + str(run_ttest()))
