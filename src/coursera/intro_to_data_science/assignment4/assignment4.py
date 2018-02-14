dde54751adf0d692848880a96271cf3ecc4ce9f7
JasonFaas <jason.a.faas@gmail.com>
2018-02-14 14:46:21 -0800

    assignment 4: q1

--- src/coursera/intro_to_data_science/assignment4/assignment4.py
+++ src/coursera/intro_to_data_science/assignment4/assignment4.py
@@ -11,101 +11,51 @@ def hello_world():
 print(hello_world())
 
 
-census_df = pd.read_csv('census.csv')
+# census_df = pd.read_csv('university_towns.txt')
 #census_df.head()
 
 
-def answer_five():
-    census_df_a5 = census_df.copy()
-    # print(str(census_df_a5['SUMLEV'].unique()))
-    census_df_a5.where(census_df_a5['SUMLEV'] == 50, inplace=True)
-    columns_to_keep = ['STNAME', 'CTYNAME']
-    #print("HELPER:\t" + str(census_df_a5['CTYNAME'].count()))
-    census_df_a5.dropna(inplace=True)
-    #print("HELPER:\t" + str(census_df_a5['CTYNAME'].count()))
-    census_df_a5 = census_df_a5[columns_to_keep]
-    # print("HELPER:\n" + str(census_df_a5.head()))
+def get_list_of_university_towns():
+    '''Returns a DataFrame of towns and the states they are in from the
+    university_towns.txt list. The format of the DataFrame should be:
+    DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ],
+    columns=["State", "RegionName"]  )
 
-    countStuff = census_df_a5.groupby(by=['STNAME']).count()
-    # print("HELPER:\t\t" + str(countStuff))
+    The following cleaning needs to be done:
 
-    return countStuff['CTYNAME'].idxmax()
+    1. For "State", removing characters from "[" to the end.
+    2. For "RegionName", when applicable, removing every character from " (" to the end.
+    3. Depending on how you read the data, you may need to remove newline character '\n'. '''
 
+    f = open('university_towns.txt', 'r')
+    message = f.read()
+    f.close()
+    split = message.split('\n')
 
-print(str(answer_five()) + "\t\t:The state with the most counties")
+    print(len(split))
 
+    df = pd.DataFrame(columns=["State", "RegionName"])
+    state = 'what'
+    for city_or_state in split:
+        if str.strip(city_or_state).endswith('[edit]'):
+            state = str.strip(city_or_state)
+            state = state[0:state.index('[')]
+            # print(state)
+        elif city_or_state.__contains__(" ("):
+            city = city_or_state[0:city_or_state.index(" (")]
+            df = df.append(pd.DataFrame([ [state, city]],columns=["State", "RegionName"]))
 
-def answer_six():
-    census_df_a6 = census_df.copy()
-    census_df_a6.where(census_df_a6['SUMLEV'] == 50, inplace=True)
-    census_df_a6.dropna(inplace=True)
-    columns_to_keep = ['STNAME', 'CENSUS2010POP']
-    census_df_a6 = census_df_a6[columns_to_keep]
-    census_df_a6.sort_values(columns_to_keep, ascending=False, inplace=True)
-    # print("HELPER:\n" + str(census_df_a6.head(8)))
-    top_3_counties_each_state = census_df_a6.groupby('STNAME').head(3)
-    top_3_total_each_state = top_3_counties_each_state.groupby(by=['STNAME']).sum()
+        # else:
+            # print("Subcontext::" + city_or_state)
 
-    top_3_states_by_county_size_df = top_3_total_each_state.sort_values(['CENSUS2010POP'], ascending=False, inplace=False).head(3)
-    # print("HELPER123:\n" + str(top_3_states_by_county_size_df))
-    top_3_as_list = top_3_states_by_county_size_df.index.values
-    # print("HELPER456:\n" + str(top_3_as_list))
-    # print("HELPER:\n" + str(census_df_a6.groupby('STNAME').head(3).head(12)))
+    # df
 
-    #print("HELPER765:\t\t" + str(type(list(top_3_as_list))))
-
-    return list(top_3_as_list)
-
-
-print(str(answer_six()) + "\t\t:The 3 states with the 3 largest counties")
+    # what_df = pd.DataFrame([["Michigan", "Ann Arbor"]],
+    # columns=["State", "RegionName"])
+    # what_df.
+    # what_df.append(["Michigan", "Yipsilanti"] )
 
 
-def answer_seven():
-    census_df_a7 = census_df.copy()
-    census_df_a7.where(census_df_a7['SUMLEV'] == 50, inplace=True)
-    census_df_a7.dropna(inplace=True)
-    columns_to_keep = [
-        'STNAME',
-        'CTYNAME',
-        'POPESTIMATE2010',
-        'POPESTIMATE2011',
-        'POPESTIMATE2012',
-        'POPESTIMATE2013',
-        'POPESTIMATE2014',
-        'POPESTIMATE2015'
-        ]
-    census_df_a7 = census_df_a7[columns_to_keep]
-    census_df_a7['max_pop_est'] = census_df_a7.max(axis=1)
-    census_df_a7['min_pop_est'] = census_df_a7.min(axis=1)
-    census_df_a7['abs_diff_est'] = census_df_a7['max_pop_est'] - census_df_a7['min_pop_est']
-    idxmax_diff = census_df_a7['abs_diff_est'].idxmax()
+    return df
 
-    # print("HELPER432:\t" + str(census_df_a7.head(8)))
-    return census_df_a7.loc[idxmax_diff]['CTYNAME']
-
-
-print(str(answer_seven()) + "\t\t:The county with the largest fluctuation during 2010 through 2015")
-
-
-def answer_eight():
-    census_df_a8 = census_df.copy()
-    census_df_a8.where(census_df_a8['SUMLEV'] == 50, inplace=True)
-    census_df_a8.dropna(inplace=True)
-    census_df_a8.where(census_df_a8['REGION'] != 3, inplace=True)
-    census_df_a8.dropna(inplace=True)
-    census_df_a8.where(census_df_a8['REGION'] != 4, inplace=True)
-    census_df_a8.dropna(inplace=True)
-    census_df_a8 = census_df_a8[census_df_a8['CTYNAME'].str.contains("Washington", na=False)]
-    census_df_a8.where(census_df_a8['POPESTIMATE2015'] > census_df_a8['POPESTIMATE2014'], inplace=True)
-    census_df_a8.dropna(inplace=True)
-    columns_to_keep = [
-        'STNAME',
-        'CTYNAME'
-    ]
-    census_df_a8 = census_df_a8[columns_to_keep]
-    # print("HELPER243:\t\t" + str(census_df_a8))
-    return census_df_a8
-
-
-print(str(answer_eight()) + "\t\t:5x2 df")
-
+print("A1:" + str(get_list_of_university_towns().head()))
