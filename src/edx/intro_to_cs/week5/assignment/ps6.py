566e601119bfa5ae446500f9d5f5b1709c68c0c4
JasonFaas <jason.a.faas@gmail.com>
2018-05-01 11:21:44 -0700

    build_shift_dict V1 in main

--- src/edx/intro_to_cs/week5/assignment/ps6.py
+++ src/edx/intro_to_cs/week5/assignment/ps6.py
@@ -87,22 +87,33 @@ class Message(object):
         Returns: a COPY of self.valid_words
         '''
         return self.valid_words[:]
-        
+
     def build_shift_dict(self, shift):
         '''
         Creates a dictionary that can be used to apply a cipher to a letter.
         The dictionary maps every uppercase and lowercase letter to a
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
-        letters only.        
-        
-        shift (integer): the amount by which to shift every letter of the 
+        letters only.
+
+        shift (integer): the amount by which to shift every letter of the
         alphabet. 0 <= shift < 26
 
-        Returns: a dictionary mapping a letter (string) to 
-                 another letter (string). 
+        Returns: a dictionary mapping a letter (string) to
+                 another letter (string).
         '''
-        pass #delete this line and replace with your code here
+        dictionary_to_return = {}
+        self.add_case_shift_to_dictionary(string.ascii_lowercase, dictionary_to_return, shift)
+        self.add_case_shift_to_dictionary(string.ascii_uppercase, dictionary_to_return, shift)
+
+        return dictionary_to_return
+
+    def add_case_shift_to_dictionary(self, case, dictionary_what, shift):
+        for base_char in case:
+            char_shift = ord(base_char) + shift
+            if char_shift > ord(case[-1:]):
+                char_shift -= len(case)
+            dictionary_what[base_char] = chr(char_shift)
 
     def apply_shift(self, shift):
         '''
